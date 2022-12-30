bca9a84176170abfbe986aba49ee2175b5a44a09
Sammy Hendrick <qbnflaco@gmail.com>
2022-12-29 20:55:59 -0800

    Add registration for serviceWorker.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import {
   createRouteConfig,
 } from "@tanstack/react-router";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 const rootRoute = createRouteConfig({
   // path: "test-spa",
   component: () => (
     <>
+      <Banner />
       <div>
         <Link to="/">Home</Link> <Link to="/about">About</Link>
       </div>
@@ -18,6 +21,61 @@ const rootRoute = createRouteConfig({
     </>
   ),
 });
+
+const Banner = () => {
+  const [showReload, setShowReload] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [waitingWorker, setWaitingWorker] = useState(null);
+
+  const onSWUpdate = (registration) => {
+    setShowReload(true);
+    setWaitingWorker(registration.waiting);
+    console.log("onUpdate!");
+  };
+  const onSWSuccess = (registration) => {
+    console.log("onSuccess!");
+    setShowSuccess(true);
+  };
+  const reloadPage = () => {
+    waitingWorker?.postMessage({ type: "SKIP_WAITING" });
+    setShowReload(false);
+    window.location.reload(true);
+  };
+
+  useEffect(() => {
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://cra.link/PWA
+    serviceWorkerRegistration.register({
+      onUpdate: onSWUpdate,
+      onSuccess: onSWSuccess,
+    });
+
+    // document.onvisibilitychange = (e) => {
+    //   serviceWorkerRegistration.forceSWupdate();
+    // };
+  }, []);
+
+  return (
+    <>
+      {showReload ? (
+        <div>
+          Update available.{" "}
+          <button type="button" onClick={reloadPage}>
+            Reload!
+          </button>
+          <hr />
+        </div>
+      ) : null}
+      {showSuccess ? (
+        <div>
+          Available offline!
+          <hr />
+        </div>
+      ) : null}
+    </>
+  );
+};
 
 const indexRoute = rootRoute.createRoute({
   path: "/",
@@ -40,7 +98,7 @@ function App() {
 function Index() {
   return (
     <div>
-      <h3>Welcome Home!</h3>
+      <h3>Welcome Home 1!</h3>
     </div>
   );
 }
@@ -48,5 +106,9 @@ function Index() {
 function About() {
   return <div>Hello from About!</div>;
 }
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 export default App;
