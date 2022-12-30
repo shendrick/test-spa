d619cc6b183eff9a39b3126894b8e77e959281d8
Sammy Hendrick <qbnflaco@gmail.com>
2022-12-29 18:00:18 -0800

    Add router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import logo from "./logo.svg";
-import "./App.css";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import {
+  Outlet,
+  RouterProvider,
+  Link,
+  createReactRouter,
+  createRouteConfig,
+} from "@tanstack/react-router";
+
+const rootRoute = createRouteConfig({
+  component: () => (
+    <>
+      <div>
+        <Link to="/">Home</Link> <Link to="/about">About</Link>
+      </div>
+      <hr />
+      <Outlet />
+    </>
+  ),
+});
+
+const indexRoute = rootRoute.createRoute({
+  path: "/",
+  component: Index,
+});
+
+const aboutRoute = rootRoute.createRoute({
+  path: "/about",
+  component: About,
+});
+
+const routeConfig = rootRoute.addChildren([indexRoute, aboutRoute]);
+
+const router = createReactRouter({ routeConfig });
 
 function App() {
+  return <RouterProvider router={router} />;
+}
+
+function Index() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Welcome Home!</h3>
     </div>
   );
+}
+
+function About() {
+  return <div>Hello from About!</div>;
 }
 
 export default App;
